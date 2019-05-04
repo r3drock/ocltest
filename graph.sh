2084494fbf06daedf69d103a4ab2e17a4d922c08
redroc <38569066+redroc@users.noreply.github.com>
2019-05-04 09:10:21 +0200

    make graph nicer

--- graph.sh
+++ graph.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Index,Time,TimePerElement" > b.csv
-./cmake-build-debug/ocltest | awk '{print $2 ","$4 "," $6}' >> b.csv
+./cmake-build-debug/ocltest | tail -n +300 |  awk '{print $2 ","$4 "," $6}' >> b.csv
 ./graph.py
 viewnior c.png
