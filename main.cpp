a3225020f4322fa08577f2a6ac176417efe0603a
r3drock <philipp.koppenstein@udo.edu>
2019-08-09 17:52:43 +0200

    add serial sepconv

--- main.cpp
+++ main.cpp
@@ -53,35 +53,160 @@ cl_program programfloat4;
 
 int min(int index, int f4);
 
-const int COUNT = 9;
-const int COUNT2 = 10;
+const int COUNT2 = 42;
+
+#define LINEAR_3(i1, i2, i3, d2, d3) ((i1) * (d2) * (d3) + (i2) * (d3) + (i3))
+#define LINEAR_4(i1, i2, i3, i4, d2, d3, d4) ((i1) * (d2) * (d3) * (d4) + (i2) * (d3) * (d4) + (i3) * (d4) + (i4))
+//separable_conv2d_6_internal_1
+
+void conv_serial_cpu()
+{
+}
+
+void sepconv_serial_cpu()
+{	
+	const int H = 121;
+    const int W = 161;				
+    const int C_IN = 3;				
+    const int C_OUT = 12;			
+    const int W_OUT = 80;			
+    const int SH = 2;				
+    const int SW = 2;				
+    const int KH = 3;				
+    const int KW = 3;				
+    const int DEPTH_MULTIPLIER = 4;	
+    
+    const int IN_OFFSET = 0;
+    const int OUT_OFFSET = 0;
+ 
+	for (int ix = 0; ix < H - KH + 1; ix += SH)
+	{
+		int x_out_1 = ix / SH;
+		for (int jx = 0; jx < W - KW + 1; jx += SW)
+		{
+			int x_out_2 = jx / SW;
+			for (int iw = 0; iw < KH; iw++)
+			{
+				int x_1 = ix + iw;
+				for (int jw = 0; jw < KW; jw++)
+				{
+					int x_2 = jx + jw;
+					for (int c = 0; c < C_IN; c++)
+					{
+						for (int m = 0; m < DEPTH_MULTIPLIER; m++)
+						{
+							int c_out = c * DEPTH_MULTIPLIER + m;
+							std::cout << LINEAR_3(x_out_1, x_out_2, c_out,
+									W_OUT, C_OUT) + OUT_OFFSET << " " << LINEAR_4(iw, jw, c, m, KW, C_IN, DEPTH_MULTIPLIER) << " " << LINEAR_3(x_1, x_2, c, W, C_IN) + IN_OFFSET << "\n";
+							//out[LINEAR_3(x_out_1, x_out_2, c_out, W_OUT, C_OUT) + OUT_OFFSET] += weights[LINEAR_4(iw, jw, c, m, KW, C_IN, DEPTH_MULTIPLIER)] * in[LINEAR_3(x_1, x_2, c, W, C_IN) + IN_OFFSET];
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int chooseDevice(cl_platform_id* platform_ids,
+		const int chosendevice,
+		cl_device_id* device_id)
+{
+	cl_device_id * device_ids;
+	cl_uint numDevices; 
+	errNum = clGetDeviceIDs(platform_ids[0], CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
+	if (errNum != CL_SUCCESS)
+	{
+		printf("Error: Failed to create a device group!\n");
+		return -1;
+	}
+
+	device_ids = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
+	errNum = clGetDeviceIDs(platform_ids[0], CL_DEVICE_TYPE_ALL, numDevices, device_ids, NULL);
+
+	std::size_t paramValueSize;
+
+	errNum = clGetDeviceInfo(
+			device_ids[chosendevice],
+			CL_DEVICE_TYPE,
+			0,
+			NULL,
+			&paramValueSize);
+	if (errNum != CL_SUCCESS)
+	{
+		std::cerr << "Failed to find OpenCL device info " << "." << std::endl;
+		return -1;
+	}
+
+	cl_device_type * info = (cl_device_type *)alloca(sizeof(cl_device_type) * paramValueSize);
+	errNum = clGetDeviceInfo(
+			device_ids[chosendevice],
+			CL_DEVICE_TYPE,
+			paramValueSize,
+			info,
+			NULL);
+	if (errNum != CL_SUCCESS)
+	{
+		std::cerr << "Failed to find OpenCL device info " << "." << std::endl;
+		return -1;
+	}
+	std::cout << "------------\n" << "CPU:" << CL_DEVICE_TYPE_CPU << "\n------------" <<std::endl;
+	std::cout << "------------\n" << "GPU:" << CL_DEVICE_TYPE_GPU << "\n------------" <<std::endl;
+	std::cout << "------------\n" << info[0] << "------------" <<std::endl;
+
+	std::cout << "Device ids:\n";
+	for (int i = 0; i < numDevices; i++)  {
+		std::cout << i << ": " << device_ids[i] << std::endl;
+	}
+	*device_id = device_ids[chosendevice];
+	return 0;
+}
 
 int initOcl()
 {
-	const char * kernelSourceString[COUNT] = {
-		"  __kernel void testkernel(\n",
-		"    __global const int *in,\n",
-		"    __global int *out)\n",
-		"  {\n",
-		"      int index = get_global_id(0); //H_OUT\n",
-		"      int temp = in[index];\n",
-		"      temp *= 2.0f;\n",
-		"      out[index] = temp;\n",
-		"  }\n"
-	};
-
-	const char * float4SourceString[COUNT2] = {
-		"__kernel void testkernelfloat4(\n",
-		"	__global const float4 * in, \n",
-		"	__global float4 * out\n",
-		")\n",
+	const char * kernelSourceString[COUNT2] = {
+		"#define LINEAR_3(i1, i2, i3, d2, d3) ((i1) * (d2) * (d3) + (i2) * (d3) + (i3))\n",
+		"#define LINEAR_4(i1, i2, i3, i4, d2, d3, d4) ((i1) * (d2) * (d3) * (d4) + (i2) * (d3) * (d4) + (i3) * (d4) + (i4))\n",
+		"__kernel void sepconv_serial(\n",
+		"  __global const float *in,\n",
+		"  __global float *out,\n",
+		"  __global float *weights)\n",
 		"{\n",
-		"      int index = get_global_id(0); //H_OUT\n",
-		"      float4 temp = in[index];\n",
-		"      temp *= 2.0f;\n",
-		"      out[index] = temp;\n",
+		"	const int W = 161;				\n",
+		"	const int C_IN = 3;				\n",
+		"	const int C_OUT = 12;			\n",
+		"	const int W_OUT = 80;			\n",
+		"	const int SH = 2;				\n",
+		"	const int SW = 2;				\n",
+		"	const int KH = 3;				\n",
+		"	const int KW = 3;				\n",
+		"	const int DEPTH_MULTIPLIER = 4;	\n",
+		"	\n",
+		"	const int IN_OFFSET = 0;\n",
+		"	const int OUT_OFFSET = 0;\n",
+		"   \n",
+		"	int x_out_1 = get_global_id(0);\n",
+		"	int x_out_2 = get_global_id(1);\n",
+		"   \n",
+		"	int ix = x_out_1 * SH;\n",
+		"	int jx = x_out_2 * SW;\n",
+		"	for (int iw = 0; iw < KH; iw++)\n",
+		"	{\n",
+		"		int x_1 = ix + iw;\n",
+		"		for (int jw = 0; jw < KW; jw++)\n",
+		"		{\n",
+		"			int x_2 = jx + jw;\n",
+		"			for (int c = 0; c < C_IN; c++)\n",
+		"			{\n",
+		"				for (int m = 0; m < DEPTH_MULTIPLIER; m++)\n",
+		"				{\n",
+		"					int c_out = c * DEPTH_MULTIPLIER + m;\n",
+		"					out[LINEAR_3(x_out_1, x_out_2, c_out, W_OUT, C_OUT) + OUT_OFFSET] += weights[LINEAR_4(iw, jw, c, m, KW, C_IN, DEPTH_MULTIPLIER)] * in[LINEAR_3(x_1, x_2, c, W, C_IN) + IN_OFFSET];\n",
+		"				}\n",
+		"			}\n",
+		"		}\n",
+		"	}\n",
 		"}\n"
-	};
+		};
 
 
 	cl_uint dev_cnt = 0;
@@ -91,17 +216,10 @@ int initOcl()
 	clGetPlatformIDs(dev_cnt, platform_ids, NULL);
 
 	cl_device_id device_id;
-	cl_device_id device_ids[10];
-	errNum = clGetDeviceIDs(platform_ids[0], CL_DEVICE_TYPE_ALL, 10, device_ids, NULL);
-	device_id = device_ids[0];
-
-	for (int i = 0; i < 10; i++)  {
-		std::cout << device_ids[i] << std::endl;
-	}
-	if (errNum != CL_SUCCESS)
-	{
-		printf("Error: Failed to create a device group!\n");
-		return -1;
+	const int chosendevice = 0;
+	int retval = 0;
+	if ((retval = chooseDevice(platform_ids, chosendevice, &device_id))) {
+			return(retval);
 	}
 
 	context = clCreateContext(0, 1, &device_id, NULL, NULL, &errNum);
@@ -118,41 +236,25 @@ int initOcl()
 		return -1;
 	}
 
-	program = clCreateProgramWithSource(context, COUNT, (const char **) kernelSourceString, NULL, &errNum);
+	program = clCreateProgramWithSource(context, COUNT2, (const char **) kernelSourceString, NULL, &errNum);
 
 	if (!program)
 	{
-		printf("Error: Failed to create compute program!\n");
+		printf("Error: Failed to create compute program! errnum: %d\n", errNum);
 		return EXIT_FAILURE;
 	}
-	programfloat4 = clCreateProgramWithSource(context, COUNT2, (const char **) float4SourceString, NULL, &errNum);
-	if (!programfloat4)
-	{
-		printf("Error: Failed to create compute program!\n");
-		return EXIT_FAILURE;
-	}
+
 
 	errNum = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
 	if (errNum != CL_SUCCESS)
 	{
 		size_t len;
 		char buffer[2048];
-		printf("Error: Failed to build program executable!\n");
+		printf("Error: Failed to build program executable! errnum: %d\n", errNum);
 		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
 		printf("%s\n", buffer);
 		exit(1);
 	}
-	errNum = clBuildProgram(programfloat4, 0, NULL, NULL, NULL, NULL);
-	if (errNum != CL_SUCCESS)
-	{
-		size_t len;
-		char buffer[2048];
-		printf("Error: Failed to build float4program executable!\n");
-		clGetProgramBuildInfo(programfloat4, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
-		printf("%s\n", buffer);
-		exit(1);
-	}
-
 	return errNum;
 }
 
@@ -166,6 +268,54 @@ int createKernel(cl_program  *program, cl_kernel *kernel, char kernelName[])
 		return -1;
 	}
 	return 0;
+}
+
+void cnn(cl_mem in_0, cl_mem weights, cl_mem out_0, size_t dim, cl_program * prog, char* name)
+{
+	{
+		{
+			cl_kernel layer;
+			size_t localWorkSize[1] = {1};
+			size_t globalWorkSize[1] = {dim};
+			{
+
+				createKernel(prog, &layer, name);
+
+				if (in_0 == NULL) {
+					printf("Given input memObject is null in %s.\n", name);
+					exit(-1);
+				}
+				if (weights == NULL) {
+					printf("Given weights memObject is null in %s.\n", name);
+					exit(-1);
+				}
+				if (out_0 == NULL) {
+					printf("Given output memObject is null in %s.\n", name);
+					exit(-1);
+				}
+				errNum = clSetKernelArg(layer, 0, sizeof(cl_mem), (void *) &in_0);
+				errNum |= clSetKernelArg(layer, 1, sizeof(cl_mem), (void *) &weights);
+				errNum |= clSetKernelArg(layer, 2, sizeof(cl_mem), (void *) &out_0);
+
+				if (errNum != CL_SUCCESS) {
+					printf("Error setting Kernel Arguments in %s.\n", name);
+					exit(-1);
+				}
+			}
+			{
+				// enqueue and wait for processing to end
+				errNum = clEnqueueNDRangeKernel(commandQueue, layer, 1,
+						NULL, globalWorkSize,
+						localWorkSize, 0, NULL,
+						NULL);
+				if (errNum != 0) {
+					printf("Error Status %d in clEnqueueNDRangeKerneltestkernel\n", errNum);
+					exit(-1);
+				}
+				clFinish(commandQueue);
+			}
+		}
+	}
 }
 
 void cnn(cl_mem in_0, cl_mem out_0, size_t dim, cl_program * prog, char* name)
@@ -215,142 +365,86 @@ void cnn(cl_mem in_0, cl_mem out_0, size_t dim, cl_program * prog, char* name)
 
 int main()
 {
-	const int size = 1000;
-	const int stepsize = 4;
+	const size_t IN_DIM		= 58443;
+	const size_t OUT_DIM		= 58000;
+	const size_t WEIGHTS_DIM  = 108;
 
+	const size_t IN_DIM4		= 58444 / 4;
+	const size_t OUT_DIM4		= 58000 / 4;
+	const size_t WEIGHTS_DIM4 = 108 / 4;
+
+	cl_float in[IN_DIM];
+	cl_float out[OUT_DIM];
+	cl_float weights[WEIGHTS_DIM];
+
+	cl_float4 in4[IN_DIM4]; 
+	cl_float4 out4[OUT_DIM4];
+	cl_float4 weights4[WEIGHTS_DIM4];
+
+	for (int i = 0; i < IN_DIM; ++i) {
+		in[i] = static_cast<cl_float>(i);
+	}
+	for (int i = 0; i < WEIGHTS_DIM; ++i) {
+		weights[i] = static_cast<cl_float>(i);
+	}
+
+	for (int i = 0; i < IN_DIM4; i+=4) {
+		in4[i/4] = (cl_float4) {static_cast<cl_float>(i), static_cast<cl_float>(i+1), static_cast<cl_float>(i+2), static_cast<cl_float>(i+3)};
+	}
+	for (int i = 0; i < OUT_DIM4; i+=4) {
+		weights4[i/4] = (cl_float4) {static_cast<cl_float>(i), static_cast<cl_float>(i+1), static_cast<cl_float>(i+2), static_cast<cl_float>(i+3)};
+	}
 
 	if (initOcl()) {
 		fprintf(stderr,"Failed InitOcl\n");
 		exit(-1);
 	}
 	//initclmemobjects();
-	cl_float *in;
-	cl_float *out;
-	cl_float4 *in4;
-	cl_float4 *out4;
-	size_t IN_DIM;
-	size_t OUT_DIM;
-	int total_elapsed = 0;
 
-	for (OUT_DIM = IN_DIM = 4; IN_DIM <= size; OUT_DIM = IN_DIM += stepsize) {
-		for (int j = 0; j < 100; ++j) {
-			in = (cl_float *) malloc(IN_DIM * sizeof(cl_float));
-			out = (cl_float *) malloc(OUT_DIM * sizeof(cl_float));
-
-			for (size_t i = 0; i < IN_DIM; i++) {
-				in[i] = static_cast<cl_int>(i);
-			}
-			cl_mem cl_in;
-			cl_mem cl_out;
-			cl_in = clCreateBuffer(context,
-					CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-					IN_DIM * sizeof(cl_float), in, &errNum);
-			cl_out = clCreateBuffer(context,
-					CL_MEM_READ_WRITE,
-					OUT_DIM * sizeof(cl_float), NULL, &errNum);
-			// reset out
-			for (size_t i = 0; i < OUT_DIM; i++) {
-				out[i] = 0.0f;
-			}
-			char name[] = "testkernel";
-
-			// run function
-			CNN_STOPWATCH(std::to_string(IN_DIM).c_str()) {
-				cnn(cl_in, cl_out, IN_DIM, &program, name);
-			}
-			// get memory from gpu
-			errNum = clEnqueueReadBuffer(commandQueue,
-					cl_out,
-					CL_TRUE,
-					0,
-					OUT_DIM * sizeof(float),
-					out,
-					0,
-					NULL,
-					NULL);
-			if (errNum != 0) {
-				printf("Error Status %d in clEnqueueReadBuffer\n", errNum);
-				exit(-1);
-			}
-			clReleaseMemObject(cl_in);
-			clReleaseMemObject(cl_out);
-			free(in);
-			free(out);
-
-			/////////////////////////////////////////
-			
-			in4 = (cl_float4 *) malloc((IN_DIM/4) * sizeof(cl_float4));
-			out4 = (cl_float4 *) malloc((OUT_DIM/4) * sizeof(cl_float4));
-			if (in4 == NULL || out4 == NULL) {
-				printf("Not enough memory!\n");
-				exit(-1);
-			}
-
-			for (size_t i = 0; i < (IN_DIM / 4); i++) {
-				in4[i] = (cl_float4){static_cast<cl_float>(i),
-					static_cast<cl_float>(i+1),
-					static_cast<cl_float>(i+2),
-					static_cast<cl_float>(i+3)};
-			}
-			cl_mem cl_in4;
-			cl_mem cl_out4;
-			size_t size = (IN_DIM / 4) * sizeof(cl_float4);
-			cl_in4 = clCreateBuffer(context,
-					CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-					size, in4, &errNum);
-			if (errNum != 0) {
-				printf("%lu %d Error Status %d in clCreateBuffer\n", size, sizeof(cl_float4),errNum);
-				exit(-1);
-			}
-			cl_out4 = clCreateBuffer(context,
-					CL_MEM_READ_WRITE,
-					(OUT_DIM / 4) * sizeof(cl_float4), NULL, &errNum);
-			if (errNum != 0) {
-				printf("Error Status %d in clCreateBuffer\n", errNum);
-				exit(-1);
-			}
-			// reset out
-			for (size_t i = 0; i < (OUT_DIM / 4); i++) {
-				out4[i] = (cl_float4){0.0f, 0.0f, 0.0f, 0.0f};
-			}
-
-			char namefloat4[] = "testkernelfloat4";
-			// run function
-			std::string s = " float4";
-			std::string s2 =std::to_string(IN_DIM) + s; 
-			CNN_STOPWATCH(s2.c_str()) {
-				cnn(cl_in4, cl_out4, IN_DIM / 4, &programfloat4, namefloat4);
-			}
-
-			// get memory from gpu
-			errNum = clEnqueueReadBuffer(commandQueue,
-					cl_out4,
-					CL_TRUE,
-					0,
-					OUT_DIM * sizeof(float),
-					out4,
-					0,
-					NULL,
-					NULL);
-
-			if (errNum != 0) {
-				printf("Error Status %d in clEnqueueReadBuffer\n", errNum);
-				exit(-1);
-			}
-
-			clReleaseMemObject(cl_in4);
-			clReleaseMemObject(cl_out4);
-			free(in4);
-			free(out4);
+	for (int j = 0; j < 10; ++j) {
+		cl_mem cl_in;
+		cl_mem cl_weights;
+		cl_mem cl_out;
+		cl_in = clCreateBuffer(context,
+				CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+				IN_DIM * sizeof(cl_float), in, &errNum);
+		cl_weights = clCreateBuffer(context,
+				CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+				WEIGHTS_DIM * sizeof(cl_float), weights, &errNum);
+		cl_out = clCreateBuffer(context,
+				CL_MEM_READ_WRITE,
+				OUT_DIM * sizeof(cl_float), NULL, &errNum);
+		// reset out
+		for (size_t i = 0; i < OUT_DIM; i++) {
+			out[i] = 0.0f;
 		}
-		printf("F4IN_DIM: %4.lu TIME: %7.d TIME_PER_ELEMENT: %5.lu ", IN_DIM, min(IN_DIM, 1), min(IN_DIM, 1) / IN_DIM);
-		total_elapsed += min(IN_DIM, 1);
-		printf("__IN_DIM: %4.lu TIME: %7.d TIME_PER_ELEMENT: %5.lu\n", IN_DIM, min(IN_DIM, 0), min(IN_DIM, 0) / IN_DIM);
-		total_elapsed += min(IN_DIM, 0);
+		char name[] = "sepconv_serial";
+
+		// run function
+		CNN_STOPWATCH(std::to_string(IN_DIM).c_str()) {
+			cnn(cl_in, cl_weights, cl_out, IN_DIM, &program, name);
+		}
+		// get memory from gpu
+		errNum = clEnqueueReadBuffer(commandQueue,
+				cl_out,
+				CL_TRUE,
+				0,
+				OUT_DIM * sizeof(float),
+				out,
+				0,
+				NULL,
+				NULL);
+		if (errNum != 0) {
+			printf("Error Status %d in clEnqueueReadBuffer\n", errNum);
+			exit(-1);
+		}
+		clReleaseMemObject(cl_in);
+		clReleaseMemObject(cl_out);
 	}
+		//printf("F4IN_DIM: %4.lu TIME: %7.d TIME_PER_ELEMENT: %5.lu ", IN_DIM, min(IN_DIM, 1), min(IN_DIM, 1) / IN_DIM);
+		printf("__IN_DIM: %4.lu TIME: %7.d TIME_PER_ELEMENT: %5.lu\n", IN_DIM, min(IN_DIM, 0), min(IN_DIM, 0) / IN_DIM);
 
 	//printf("time: %d nano seconds\nsize: %d\naverage: %d nano seconds/run\n", total_elapsed, IN_DIM - 1, total_elapsed/IN_DIM);
-
 	return 0;
 }
 
@@ -373,5 +467,6 @@ int min(int index, int f4) {
 	return avg;
 	return min;
 }
+
 
 
